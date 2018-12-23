872c6808cca7709206dedaa77b581e275bec254e
John Lam <jlam@iunknown.com>
2018-12-23 12:07:52 -0800

    Work in progress for simple paste command

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,27 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import Window = vscode.window;
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "writedown" is now active!');
+    console.log('writedown extension activated');
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
+    var disposables = [];
+    disposables.push(vscode.commands.registerCommand('extension.helloWorld', () => {
         vscode.window.showInformationMessage('My First Extension: Hello John!');
-    });
+    }));
 
-    context.subscriptions.push(disposable);
+    disposables.push(vscode.commands.registerCommand('writedown.paste', () => {
+        const e = Window.activeTextEditor;
+        const d = e ? e.document : null;
+        if (!e || !d) {
+            return;
+        }
+
+    }));
+
+    context.subscriptions.concat(disposables);
 }
 
 // this method is called when your extension is deactivated
