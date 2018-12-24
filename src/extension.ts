9f9ec0643ce1b9eb53124b2a75a33f77e7d5b898
John Lam <jlam@iunknown.com>
2018-12-23 17:14:58 -0800

    paste command changes any text on clipboard into 'holy moly'

--- src/extension.ts
+++ src/extension.ts
@@ -1,23 +1,61 @@
 import * as vscode from 'vscode';
 import Window = vscode.window;
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+// We use the activate() function to register the commands for the writedown extension
 export function activate(context: vscode.ExtensionContext) {
 
-    console.log('writedown extension activated');
-
+    // This is the array that we use to contain all of the disposable resources that we 
+    // are registering with VS Code.
     var disposables = [];
-    disposables.push(vscode.commands.registerCommand('extension.helloWorld', () => {
-        vscode.window.showInformationMessage('My First Extension: Hello John!');
-    }));
 
+    // Each command that we register must be registered with our disposables collection to 
+    // ensure that we have the correct cleanup semantics when we are done.
     disposables.push(vscode.commands.registerCommand('writedown.paste', () => {
-        const e = Window.activeTextEditor;
-        const d = e ? e.document : null;
-        if (!e || !d) {
+
+        // Before the paste command can run, we must have both an active editor and an active 
+        // document (and should we test to see if the document is editable?)
+        const editor = Window.activeTextEditor;
+        const document = editor ? editor.document : null;
+        if (!editor || !document) {
             return;
         }
+
+        const start = editor.selection.start;
+
+        // Run the base clipboard paste command. I believe that this command simply delegates to the
+        // Electron paste command. When the paste command returns, the VS code selection in the editor
+        // has been extended to encompass all of the text that was pasted into the editor.
+        vscode.commands.executeCommand("editor.action.clipboardPasteAction")
+            .then(() => {
+
+                // Read the text from the document *after* it has been pasted. We don't have the ability
+                // to read the text beforehand, so we must first paste, then read the pasted text before
+                // we can reformat it into something else.
+                const end = editor.selection.end;
+                let selection = new vscode.Selection(start.line, start.character, end.line, end.character);
+                let selectedText = document.getText(selection);
+
+                // TODO: write a new class that handles examining the inserted text from the clipboard 
+                // and replacing it with the right thing. 
+
+                // TODO: if we want to examine the clipboard before the paste operation, e.g., detect 
+                // the presence of a bitmap image on the clipboard, we will need to use some cross platform
+                // node.js extensions to read from the clipboard.
+
+                // TODO: understand how to incorporate 3rd party node.js extensions into our VS Code 
+                // extension
+
+                // Do something silly: replace the selection with "holy moly"
+                editor.edit(edit => {
+                    edit.replace(selection, 'holy moly');
+                }, 
+                { 
+                    undoStopAfter: false, 
+                    undoStopBefore: false 
+                });
+                
+                console.log(`pasted something: ${selectedText}`);
+            });
 
     }));
 
